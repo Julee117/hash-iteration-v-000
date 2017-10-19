53646db6eee8c6f975d249383a28ae411a394355
Julianne Kim <juliannne@gmail.com>
2017-10-19 13:55:48 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -10,6 +10,6 @@ def select_winner(passengers)
   passengers.each do |key, value|
     if key.start_with?("A")
       return value
-    end 
+    end
   end 
 end
